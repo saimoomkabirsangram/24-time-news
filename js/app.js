8ab0762a3a914686fabce1fbac7d73c35bf79534
saimoomkabirsangram <saimoomkabirsangram@gmail.com>
2022-09-03 00:54:20 +0600

    dynamic-list-added

---
+++ js/app.js
@@ -0,0 +1,23 @@
+const newsCategoriesApi = async() =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/news/categories')
+    const data = await res.json()
+    return data
+}
+
+const displayAllNewsCategories = async() => {
+    const newsCategories = await newsCategoriesApi()
+    const newsCategorie= newsCategories.data.news_category;
+    const menu = document.getElementById('news-categories')
+    for(const categorie of newsCategorie ){
+        // console.log(categorie.category_name);
+
+        const li = document.createElement('li')
+            li.innerHTML =`
+            <a class="text-decoration-none px-4" >${categorie.category_name}</a>
+            `
+            menu.appendChild(li);
+
+    }
+}
+newsCategoriesApi()
+displayAllNewsCategories()
