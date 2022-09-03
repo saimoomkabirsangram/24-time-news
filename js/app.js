dd90afe06c290bd384d600dba73f61d19e97ce2f
saimoomkabirsangram <saimoomkabirsangram@gmail.com>
2022-09-03 23:08:15 +0600

    loder-added

--- js/app.js
+++ js/app.js
@@ -27,7 +27,12 @@ const displayAllNewsCategories = async() => {
 }
 
 const LoadAllNewsDetaild = async(idCategory) =>{
-  const url = `https://openapi.programming-hero.com/api/news/category/${idCategory}`
+
+    // remove spinner 
+    const spinner = document.getElementById('spinner');
+    spinner.classList.remove('d-none');
+    
+    const url = `https://openapi.programming-hero.com/api/news/category/${idCategory}`
   try{
     fetch(url)
     .then(res => res.json())
@@ -37,17 +42,34 @@ const LoadAllNewsDetaild = async(idCategory) =>{
   }
 }
 
-const displayCatDetails = catDetails =>{
+const displayCatDetails = catDetails => {
   // console.log(cat);
+
+  // data found 
   const catCountElemnet = document.getElementById('category-found');
-  catCountElemnet.innerText = (catDetails.length);
+    catCountElemnet.innerText = (catDetails.length);
+
+  if (catDetails.length === 0) {
+    catCountElemnet.innerText = ('Sorry! No data found')
+  }
+  else {
+    catCountElemnet.innerText = (catDetails.length + ' ' + 'items Found in this category');
+  }
+
+
+
 
 
   const categoryDetails = document.getElementById('category_container');
   const div = document.createElement('div')
+
+  // spinner add 
+  spinner.classList.add('d-none')
   categoryDetails.innerHTML=`` ;
   catDetails.forEach(catDetails => {
-      const catDiv = document.createElement('div');
+    const catDiv = document.createElement('div');
+    
+    
       catDiv.classList.add('col');
       catDiv.innerHTML=`
       <div class="card mb-3 container" style="max-width: 1200px;">
@@ -66,27 +88,6 @@ const displayCatDetails = catDetails =>{
             <button type="button" class="btn-primary btn ms-5 ms-5" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
             See More
             </button>
-
-      
-            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="staticBackdropLabel">${catDetails.title ? catDetails.title: 'Not Found'}</h5>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                <span>${catDetails.author.name? catDetails.author.name: 'No author'}</span>
-                <span>View:${catDetails.total_view? catDetails.total_view:'Still no view'}</span>
-                </div>
-                <div class="modal-footer">
-                  <img src="${catDetails.author.img ? catDetails.author.img: 'Not Found'}" class="card-img-bottom img-thumbnail rounded-4" alt="...">
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary">Understood</button>
-                </div>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
@@ -100,8 +101,6 @@ const displayCatDetails = catDetails =>{
 }
 
 
-
-
 LoadAllNewsDetaild()
 newsCategoriesApi()
 displayAllNewsCategories()
@@ -112,7 +111,9 @@ displayAllNewsCategories()
 
 // blog post show 
  document.getElementById('blog-post').addEventListener('click', function(){
+
     const blog = document.getElementById('question-show')
+    blog.innerText= ``; 
     const div = document.createElement('div')
     div.innerHTML = `
     <div class="accordion col" id="accordionExample">
