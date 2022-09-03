0625f7dfa774db82e39fed00708b737e371094af
saimoomkabirsangram <saimoomkabirsangram@gmail.com>
2022-09-03 17:26:11 +0600

    Home-button-added

--- js/app.js
+++ js/app.js
@@ -65,7 +65,28 @@ const displayCatDetails = catDetails =>{
             <span class="mr-5 ps-5">View: ${catDetails.total_view ? catDetails.total_view: 'Not Found'}</span>
             <button type="button" class="btn-primary btn ms-5 ms-5" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
             See More
-          </button>
+            </button>
+
+      
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="staticBackdropLabel">${catDetails.title ? catDetails.title: 'Not Found'}</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                <span>${catDetails.author.name? catDetails.author.name: 'No author'}</span>
+                <span>View:${catDetails.total_view? catDetails.total_view:'Still no view'}</span>
+                </div>
+                <div class="modal-footer">
+                  <img src="${catDetails.author.img ? catDetails.author.img: 'Not Found'}" class="card-img-bottom img-thumbnail rounded-4" alt="...">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Understood</button>
+                </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -93,7 +114,6 @@ displayAllNewsCategories()
  document.getElementById('blog-post').addEventListener('click', function(){
     const blog = document.getElementById('question-show')
     const div = document.createElement('div')
-    div.classList.add('div')
     div.innerHTML = `
     <div class="accordion col" id="accordionExample">
     <div class="accordion-item">
